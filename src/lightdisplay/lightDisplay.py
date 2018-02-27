1a470aaf516021f90ff52e7bb875f58a9bddf7a5
SBean92 <sean.behan@ucdconnect.ie>
2018-02-27 10:09:23 +0000

    Updated action function

--- src/lightdisplay/lightDisplay.py
+++ src/lightdisplay/lightDisplay.py
@@ -5,13 +5,22 @@ class lightDisplay:
     def __init__(self, L):
         self.lights = [[False for x in range(L)] for x in range(L)]
     
-    def action(self, cmd):
+    def action(self, cmd, a, b, x, y):
         if cmd == "turn on":
-            #TO-DO
+            for i in range(a,x):
+                for j in range(b,y):
+                    self.lights[i][j] = True
         elif cmd == "turn off":
-            #TO-DO
+            for i in range(a,x):
+                for j in range(b,y):
+                    self.lights[i][j] = False
         elif cmd == "switch":
-            #TO-DO
+            for i in range(a,x):
+                for j in range(b,y):
+                    if self.lights[i][j] == True:
+                        self.lights[i][j] = False
+                    elif self.lights[i][j] == False:
+                        self.lights[i][j] = True
 
     def count(self):
         #TO-DO
