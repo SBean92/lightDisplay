58afaf1f06c6d5f16d88e4b225f0bbaa4356f0c7
SBean92 <sean.behan@ucdconnect.ie>
2018-02-27 12:59:01 +0000

    Added options for different light configs to count output

--- src/lightdisplay/lightDisplay.py
+++ src/lightdisplay/lightDisplay.py
@@ -1,23 +1,21 @@
 class lightDisplay:
 
-    lights = None
-
     def __init__(self, L):
         self.lights = [[False for x in range(L)] for y in range(L)]
         self.dimension = L
     
     def action(self, cmd, x_1, y_1, x_n, y_n):
         if cmd == "turn on":
-            for i in range(x_1,x_n):
-                for j in range(y_1,y_n):
+            for i in range(x_1,x_n+1):
+                for j in range(y_1,y_n+1):
                     self.lights[i][j] = True
         elif cmd == "turn off":
-            for i in range(x_1,x_n):
-                for j in range(y_1,y_n):
+            for i in range(x_1,x_n+1):
+                for j in range(y_1,y_n+1):
                     self.lights[i][j] = False
         elif cmd == "switch":
-            for i in range(x_1,x_n):
-                for j in range(y_1,y_n):
+            for i in range(x_1,x_n+1):
+                for j in range(y_1,y_n+1):
                     if self.lights[i][j] == True:
                         self.lights[i][j] = False
                     elif self.lights[i][j] == False:
@@ -32,7 +30,13 @@ class lightDisplay:
                     self.tCount += 1
                 elif self.lights[i][j] == False:
                     self.fCount += 1
-        print("There are",self.tCount,"lights on and",self.fCount,"lights off")
-        #TO-DO: Add statements for when there are no lights on or all lighst on so on....
-                
-        
+        if self.tCount == 0:
+            print("There are no lights on")
+        elif self.fCount == 0:
+            print("There are no lights off")
+        elif self.tCount == 1:
+            print("There is one light on and",self.fCount,"lights off")
+        elif self.fCount == 1:
+            print("There are",self.tCount,"lights on and one light off")
+        else:
+            print("There are",self.tCount,"lights on and",self.fCount,"lights off")
