0ab61e013dd7b8ee2398aef95c3728aac26ba500
SBean92 <sean.behan@ucdconnect.ie>
2018-03-02 13:16:08 +0000

    renamed some parts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Setup file for lightDisplay.
+    Setup file for solveLED.
 
     This file was generated with PyScaffold 3.0.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
@@ -15,18 +15,18 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-        name = "lightDisplay",
+        name = "solveLED",
         version = "0.1",
         description = "Displays basic system information",
         url="https://github.com/SBean92/lightDisplay",
         author = "Sean Behan",
         aurhor_email = "sean.behan@ucdconnect.ie",
         licence = "GPL3",
-        packages = ['lightDisplay'],
+        packages = ['solveLED'],
         entry_points = {
-        'console_scripts':['lightDisplay=lightDisplay.main:main']
+        'console_scripts':['solveLED=solveLED.main:main']
         },
         use_pyscaffold=True)
 
-if __name__ == "__main__":
+if __name__ == "solveLED":
     setup_package()
