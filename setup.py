c6737ae52ea2b874d9eb099deb9cefc6a4d62212
SBean92 <sean.behan@ucdconnect.ie>
2018-03-02 12:16:18 +0000

    Edited setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Setup file for lightdisplay.
+    Setup file for lightDisplay.
 
     This file was generated with PyScaffold 3.0.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
@@ -11,22 +11,22 @@
 import sys
 from setuptools import setup
 
-# Add here console scripts and other entry points in ini-style format
-entry_points = """
-[console_scripts]
-# script_name = lightdisplay.module:function
-# For example:
-# fibonacci = lightdisplay.skeleton:run
-"""
-
-
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-          entry_points=entry_points,
-          use_pyscaffold=True)
-
+        name = "lightDisplay",
+        version = "0.1",
+        description = "Displays basic system information",
+        url="https://github.com/SBean92/lightDisplay",
+        author = "Sean Behan",
+        aurhor_email = "sean.behan@ucdconnect.ie",
+        licence = "GPL3",
+        packages = ['lightDisplay'],
+        entry_points = {
+        'console_scripts':['lightDisplay=lightDisplay.main:main']
+        },
+        use_pyscaffold=True)
 
 if __name__ == "__main__":
     setup_package()
